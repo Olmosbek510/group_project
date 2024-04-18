f222ac67fa2ce9f394404dcc8e36ab37272bc515
Olmosbek510 <olmosbekorazboyev@gmail.com>
2024-04-18 23:06:07 +0500

    all configirations done

---
+++ src/main/java/uz/inha/group_project/entity/Group.java
@@ -0,0 +1,20 @@
+package uz.inha.group_project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "groups")
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Group {
+    @Id
+    private int id;
+    private String name;
+}
